5ec61b9c7ca72b37b3892c49cb7791a4140fc983
Asher Hoeveler <ahoeveler@apotechgroup.com>
2024-02-29 18:22:29 -0600

    Basic data mutation

--- Server/src/api/interfaces/gviz.ts
+++ Server/src/api/interfaces/gviz.ts
@@ -1,6 +1,9 @@
-import { UUID } from "crypto";
+import uuid from 'uuid';
+import { Database } from 'sqlite';
+import sqlite3 from 'sqlite3';
 
 
+type UUID = typeof uuid.v4;
 type GraphNode = {
     UUID:string,
     name:string,
@@ -13,10 +16,51 @@ type GraphEdge = {
 };
 
 export default class gviz {
-    private nodes:Map<UUID, GraphNode> = new Map();
-    private edges:Map<UUID, GraphEdge> = new Map();
+    private nodes:Map<string, GraphNode> = new Map();
+    private edges:Map<string, GraphEdge> = new Map();
     public gviz() {
-        this.nodes = new Map<UUID, GraphNode>();
-        this.edges = new Map<UUID, GraphEdge>()
+        this.nodes = new Map<string, GraphNode>();
+        this.edges = new Map<string, GraphEdge>();
+    }
+    public addNode(nodeName:string, connections?:GraphEdge[]):string{
+        const _uuid = uuid.v4();
+        const _connections = (connections == undefined?[]:connections).map((edge):GraphEdge => {
+            if (!this.nodes.has(edge.to.UUID))
+                throw new Error(`Node ${edge.to.UUID} does not exist`);
+            return edge;
+        });
+        this.nodes.set(_uuid, {UUID: _uuid, name:nodeName, connections: _connections});
+        for (let edge of _connections) {
+            this.nodes.get(edge.to.UUID)?.connections.push(edge);
+        }
+        for (let edge of _connections) {
+            this.edges.set(uuid.v4(), edge);
+        } 
+        return _uuid
+    }
+    public connect(nodeA:string, nodeB:string) {
+        if (!uuid.validate(nodeA)) {
+            for (let [uuid, node] of this.nodes) {
+                if (node.name == nodeA)
+                    nodeA = uuid;
+            }
+            if (!uuid.validate(nodeA))
+                throw new Error(`Could not find ${nodeA}`);
+        }
+        if (!uuid.validate(nodeB)) {
+            for (let [uuid, node] of this.nodes) {
+                if (node.name == nodeB)
+                    nodeB = uuid;
+            }
+            if (!uuid.validate(nodeB))
+                throw new Error(`Could not find ${nodeB}`);
+        }
+        //Both nodes should be valid UUIDs now
+        if (!(this.nodes.has(nodeA) && this.nodes.has(nodeB)))
+            throw new Error('Failed to create connection: could not find nodes');
+        //TODO finish connection
+    }
+    public save(db:Database<sqlite3.Database, sqlite3.Statement>) {
+        //TODO method to save to database
     }
 }

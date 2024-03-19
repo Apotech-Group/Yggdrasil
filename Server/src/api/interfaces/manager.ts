ffe6145d86f56df4fe2e46eed8822794fe6ff849
Asher Hoeveler <ahoeveler@apotechgroup.com>
2024-03-19 18:00:13 -0500

    Start rebuild (Why wasn't the database built into the manager, am I stupid)

--- Server/src/api/interfaces/manager.ts
+++ Server/src/api/interfaces/manager.ts
@@ -1,136 +1,78 @@
-import * as uuid from 'uuid';
-import { Database } from 'sqlite';
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
+import { Database, open as dbOpen } from "sqlite";
+import assert from "assert";
 
-type GraphNode = {
-    UUID: string,
-    name: string,
-    connections: GraphEdge[],
-    deprecated: boolean,
-    serial:string,
-    location:string,
-};
-type GraphEdge = {
-    from: GraphNode,
-    to: GraphNode,
-    type: "CAT4" | "CAT5" | "CAT6" | "Fiber" | "Virtual",
-};
+type DBRow = {
+    ID:string,
+}
+type XC = DBRow & {
+    FNODE: string,
+    FPORT: number,
+    TNODE: string,
+    TPORT: number
+}
+type NODE = DBRow & {
+    SERIALNUM: string,
+    LOCSTRING: string,
+    PORTCOUNT: number
+}
+type DatabaseReference = Database<sqlite3.Database, sqlite3.Statement>;
+export default class Manager {
+    protected database?:Database<sqlite3.Database, sqlite3.Statement>;
+    private constructor() {}
+    public static create():Promise<Manager> {
+        return new Promise<Manager>(async (resolve, reject) => {
+            const _manager = new Manager();
+            _manager.database = await dbOpen({
+                driver: sqlite3.Database,
+                filename: 'datastore.db'
+            });
+            //set WAL
+            console.log(await _manager.database.get('PRAGMA journal_mode=WAL'));
+            assert.ok(typeof _manager.database !== "undefined", "Failed to create or open database");
+            await _manager.database.exec('BEGIN TRANSACTION;');
 
-export default class manager {
-    private nodes: Map<string, GraphNode> = new Map();
-    private edges: Map<string, GraphEdge> = new Map();
-    /**
-     * Instantiates a new graph manager
-     */
-    public manager() {
-        this.nodes = new Map<string, GraphNode>();
-        this.edges = new Map<string, GraphEdge>();
-    }
-    /**
-     * Instantiates a new graph manager from a sqlite database
-     * @param db Database to load the existing connections from
-     */
-    public static async fromDB(db:Database<sqlite3.Database, sqlite3.Statement>) {
-        const inst = new manager();
-        const savedNodes: {ID:string, nodeName:string, Deprecated:boolean, SerialNum:string, nodeLocation: string}[] = await db.all(/*sql*/`SELECT * FROM NODEDB;`);
-        const savedEdges: {XCID: string, startID:string, endID:string, runType:GraphEdge['type'], runLength: string, SOF:string}[] = await db.all(/*sql*/`SELECT * FROM CCDB;`);
-        savedNodes.forEach((node) => {
-            inst.addNode(node.nodeName, [], node.nodeLocation, node.SerialNum, node.ID, node.Deprecated);
+            [
+                /// Cross-connect database
+                /*sql*/`CREATE TABLE XCDB (
+                    ID TEXT NOT NULL PRIMARY KEY,   -- UUID V4 of this CC
+                    FNODE TEXT NOT NULL,            -- UUID V4 of FROM node
+                    FPORT INTEGER,                  -- Connectd port number [FROM node, NULL if shared or virtual]
+                    TNODE TEXT NOT NULL,            -- UUID V4 of TO node
+                    TPORT INTEGER,                  -- Conencted port number [TO node, NULL if shared or virtual]
+                );`,
+                /// Network node database
+                /*sql*/`CREATE TABLE NODE (
+                    ID TEXT NOT NULL PRIMARY KEY,   -- UUID V4
+                    SERIALNUM TEXT NOT NULL UNIQUE, -- Serial of the device
+                    LOCSTRING TEXT NOT NULL UNIQUE, -- Format: [Rackstring]:[shelfStart]-[shelfEnd]
+                    PORTCOUNT INTEGER,              -- Number of available ports on device, NULL=Unlimited [virtual]
+                );`
+            ].forEach(async statement => await _manager.database!.exec(statement));
+
+            await _manager.database.exec('COMMIT;');
         });
-        savedEdges.forEach((edge) => {
-            inst.connect(edge.startID, edge.endID, edge.runType, edge.XCID);
-        });
-        return inst;
     }
-    /**
-     * @param {string} nodeName - What to name the added network node
-     * @param {Array<GraphEdge>} connections - An array of connections bound to the node.
-     * @param {string} locString - A string with the format [dc-row-rack:shelfStart-shelfEnd] describing the node's location
-     * @param {string} serial - A serial, VMID, etc describing this specific network node
-     * @param {string} id - If loading, the uuid to assign this node
-     * @param {boolean} deprecated - Whether additional runs should be made to this device
-     * @returns {string} UUID of the added node
-     */
-    public addNode(nodeName: string, connections: GraphEdge[] = [], locString: string, serial: string, id?:string, deprecated:boolean=false): string {
-        const _uuid = id || uuid.v4();
-        const _connections = connections.map((edge): GraphEdge => {
-            if (!this.nodes.has(edge.to.UUID))
-                throw new Error(`Node ${edge.to.UUID} does not exist`);
-            return edge;
-        });
-        this.nodes.set(_uuid, { UUID: _uuid, name: nodeName, connections: _connections, deprecated: deprecated, location: locString, serial: serial });
-        for (let edge of _connections) {
-            this.nodes.get(edge.to.UUID)?.connections.push(edge);
-        }
-        for (let edge of _connections) {
-            this.edges.set(uuid.v4(), edge);
-        }
-        return _uuid
-    }
-    /**
-     * 
-     * @param {string} nodeA UUID or name of the primary node
-     * @param {string} nodeB UUID or name of the secondary node
-     * @param {GraphEdge['type']} cableType Type of cable run
-     * @param {string} id If loading, the uuid to assing this connection
-     * @returns {string} UUID of the added connection
-     */
-    public connect(nodeA: string, nodeB: string, cableType: GraphEdge['type'], id?: string): string {
-        if (!uuid.validate(nodeA)) {
-            for (let [uuid, node] of this.nodes) {
-                if (node.name == nodeA)
-                    nodeA = uuid;
-            }
-            if (!uuid.validate(nodeA))
-                throw new Error(`Could not find ${nodeA}`);
-        }
-        if (!uuid.validate(nodeB)) {
-            for (let [uuid, node] of this.nodes) {
-                if (node.name == nodeB)
-                    nodeB = uuid;
-            }
-            if (!uuid.validate(nodeB))
-                throw new Error(`Could not find ${nodeB}`);
-        }
-        //Both nodes should be valid UUIDs now
-        if (!(this.nodes.has(nodeA) && this.nodes.has(nodeB)))
-            throw new Error('Failed to create connection: could not find nodes');
-        let _uuid = id || uuid.v4();
-
-        this.edges.set(_uuid, {
-            from: this.nodes.get(nodeA)!,//HACK this is type safe becuase of validation above
-            to: this.nodes.get(nodeB)!,  //HACK this is type safe becuase of validation above
-            type: cableType
-        });
-        this.nodes.get(nodeA)!.connections.push(this.edges.get(_uuid)!);
-        this.nodes.get(nodeB)!.connections.push(this.edges.get(_uuid)!);
-
-        return _uuid;
-    }
-
-    public async save(db: Database<sqlite3.Database, sqlite3.Statement>) {
-        let nodeStatement = '';
-        let edgeStatement = '';
-        {
-            const existingNodes = await db.all(/*sql*/`SELECT (ID) FROM NODEDB;`);
-            const existingEdges = await db.all(/*sql*/`SELECT (XCID) FROM CCDB;`);
-            console.log(existingNodes);
-            console.log(existingEdges);
-            for (let id of this.nodes.keys()) {
-                if (!existingNodes.includes(id))
-                    nodeStatement += `${(nodeStatement.length == 0 ? '' : ',\n')}(
-                        '${id}',
-                        '${this.nodes.get(id)?.name}',
-                        ${this.nodes.get(id)?.deprecated ? 'TRUE':'FALSE'},
-                        '${this.nodes.get(id)?.serial}',
-                        '${this.nodes.get(id)?.location}'
-                    )`;
+    private construct = {
+        fetcher<T extends DBRow>(dbref:DatabaseReference, tableName:string, column:string):(searchvalue: string) => Promise<(T)[] | null> {
+            return async (searchvalue:string) => {
+                return (await dbref.all(`SELECT * FROM ${tableName} WHERE ${column} = "${searchvalue}"`) || null)
             }
         }
-        {
-            console.debug(nodeStatement+'\n'+edgeStatement);
-            await db.exec(/*sql*/`INSERT INTO NODEDB (ID, nodeName, Deprecated, SerialNum, nodeLocation) VALUES ${nodeStatement}`);
-            // await db.exec(/*sql*/`INSERT INTO CCDB (XCID, startID, endID, runType, runLength, SOF) VALUES`)
+    }
+    public get = {
+        node: {
+            byUUID:this.construct.fetcher<NODE>(this.database!, 'NODE', 'ID'),
+            bySerial:this.construct.fetcher<NODE>(this.database!, 'NODE', 'SERIALNUM'),
+            byLocation: this.construct.fetcher<NODE>(this.database!, 'NODE', 'LOCSTRING'),
+        },
+        xc: {
+            byUUID:this.construct.fetcher<XC>(this.database!, 'XCDB', 'ID'),
+            byStartNode: this.construct.fetcher<XC>(this.database!, 'XCDB', 'FNODE'),
+            byEndNode: this.construct.fetcher<XC>(this.database!, 'XCDB', 'TNODE'),
+            async byConnected(nodeid:string):Promise<(XC)[]> {
+                return (await this.byStartNode(nodeid) || []).concat(await this.byEndNode(nodeid) || []);
+            }
         }
     }
 }
--- Server/src/api/v1.ts
+++ Server/src/api/v1.ts
@@ -5,8 +5,9 @@ import express, { RequestHandler } from "express";
 
 import sqlite3 from "sqlite3";
 import { Database, open as dbOpen } from "sqlite";
-import manager from "./interfaces/manager";
+import Manager from "./interfaces/manager";
 import * as uuid from 'uuid';
+import assert from "assert";
 
 const blobScalar = new GraphQLScalarType({
     name: "BLOB",
@@ -111,83 +112,75 @@ export class v1 {
 
     public middleware: RequestHandler = () => { };
     public database: Database<sqlite3.Database, sqlite3.Statement> | undefined;
-    public ccmanager:manager = new manager();//ignore this it gets reassigned
+    public ccmanager?:Manager//ignore this it gets reassigned
 
     public constructor(onReady: () => void) {
         this.ready = false;
         console.log('Entering IIFE');
         (async () => {
             console.log('opening db');
-            this.database = await dbOpen({
-                driver: sqlite3.Database,
-                filename: "crossconnects.db"
-            });
-            console.log('Setting WAL mode');
-            await this.database.exec(`PRAGMA journal_mode=WAL;`);
-            console.log('Instantiating GQL server');
-            await this.database?.exec(/*sql*/`SELECT * FROM NODEDB LIMIT 1;`).catch(async (e) => {
-                process.stdout.write('Node Database not instantiated, running declaration:  ');
-                await this.database?.exec(/*sql*/`
-                CREATE TABLE NODEDB (
-                    --UUID
-                    ID TEXT PRIMARY KEY NOT NULL,
-                    --Name/ref#/etc (SHOULD NOT BE BASED ON LOCATION)
-                    nodeName TEXT NOT NULL,
-                    --Whether or not additional cross connects should be run to this node
-                    Deprecated Boolean NOT NULL DEFAULT False,
-                    --Serial number
-                    SerialNum TEXT NOT NULL UNIQUE,
-                    --Location string, I.E. DC-A-R-01-01:14-17 for DC-A Row 1 Rack 1 units 14 through 17
-                    nodeLocation TEXT NOT NULL
-                );`);
-                console.log('Done');
-            }).finally(() => {
-                console.log('Node Database loaded');
-            });
-            await this.database.exec(/*sql*/`SELECT * FROM CCDB LIMIT 1;`).catch(async (e) => {
-                process.stdout.write('Edge Database not instantiated, running declaration:  ');
-                await this.database?.exec(/*sql*/`
-                CREATE TABLE CCDB (
-                    --UUID
-                    XCID TEXT PRIMARY KEY NOT NULL,
-                    --ID of start Node
-                    startID INTEGER NOT NULL,
-                    --ID of end Node
-                    endID INTEGER NOT NULL,
-                    --Should be CatX | Fiber, enforced on API
-                    runType TEXT NOT NULL,
+            // this.database = await dbOpen({
+            //     driver: sqlite3.Database,
+            //     filename: "crossconnects.db"
+            // });
+            // console.log('Setting WAL mode');
+            // await this.database.exec(`PRAGMA journal_mode=WAL;`);
+            // console.log('Instantiating GQL server');
+            // await this.database?.exec(/*sql*/`SELECT * FROM NODEDB LIMIT 1;`).catch(async (e) => {
+            //     process.stdout.write('Node Database not instantiated, running declaration:  ');
+            //     await this.database?.exec(/*sql*/`
+            //     CREATE TABLE NODEDB (
+            //         --UUID
+            //         ID TEXT PRIMARY KEY NOT NULL,
+            //         --Name/ref#/etc (SHOULD NOT BE BASED ON LOCATION)
+            //         nodeName TEXT NOT NULL,
+            //         --Whether or not additional cross connects should be run to this node
+            //         Deprecated Boolean NOT NULL DEFAULT False,
+            //         --Serial number
+            //         SerialNum TEXT NOT NULL UNIQUE,
+            //         --Location string, I.E. DC-A-R-01-01:14-17 for DC-A Row 1 Rack 1 units 14 through 17
+            //         nodeLocation TEXT NOT NULL
+            //     );`);
+            //     console.log('Done');
+            // }).finally(() => {
+            //     console.log('Node Database loaded');
+            // });
+            // await this.database.exec(/*sql*/`SELECT * FROM CCDB LIMIT 1;`).catch(async (e) => {
+            //     process.stdout.write('Edge Database not instantiated, running declaration:  ');
+            //     await this.database?.exec(/*sql*/`
+            //     CREATE TABLE CCDB (
+            //         --UUID
+            //         XCID TEXT PRIMARY KEY NOT NULL,
+            //         --ID of start Node
+            //         startID INTEGER NOT NULL,
+            //         --ID of end Node
+            //         endID INTEGER NOT NULL,
+            //         --Should be CatX | Fiber, enforced on API
+            //         runType TEXT NOT NULL,
 
-                    --Length stored as [..feet][inches%2d]
-                    runLength INTEGER NOT NULL,
+            //         --Length stored as [..feet][inches%2d]
+            //         runLength INTEGER NOT NULL,
 
-                    --File upload of SOF
-                    SOF BLOB NOT NULL,
-                    FOREIGN KEY(startID) REFERENCES NODEDB(ID)
-                    FOREIGN KEY(endID) REFERENCES NODEDB(ID)
-                );`);
-                console.log('Done');
-            }).finally(() => {
-                console.log('Edge Database loaded');
-            });
+            //         --File upload of SOF
+            //         SOF BLOB NOT NULL,
+            //         FOREIGN KEY(startID) REFERENCES NODEDB(ID)
+            //         FOREIGN KEY(endID) REFERENCES NODEDB(ID)
+            //     );`);
+            //     console.log('Done');
+            // }).finally(() => {
+            //     console.log('Edge Database loaded');
+            // });
             const server = new ApolloServer<{}>({
                 typeDefs: schema,
                 resolvers: resolvers
             });
             await server.start();
             console.log('Instantiating graph manager');
-            this.ccmanager = await manager.fromDB(this.database);
+            this.ccmanager = await Manager.create();
+            assert.ok(typeof this.ccmanager !== 'undefined', "Could not instantiate cc manager");
             console.log('Finishing up');
             this.middleware = expressMiddleware(server);
             this.ready = true;
-            //DEBUG START
-            console.debug('DEBUG START');
-            console.log(uuid.v4());
-            let nodeA = this.ccmanager.addNode('testnode', [], 'test location', '000001');
-            let nodeB = this.ccmanager.addNode('testnode2', [], 'test location', '000002');
-            this.ccmanager.connect(nodeA, nodeB, 'Virtual');
-            this.ccmanager.save(this.database);
-            console.debug('DEBUG END');
-            //DEBUG END
             console.log('Firing callback');
             onReady();
         })();

f208c1cdafb88e160737c473d89791b546c349b4
Asher Hoeveler <ahoeveler@apotechgroup.com>
2024-03-25 18:22:27 -0500

    Setter creation function
    might be broken but god I hope it isn't :)

--- Server/src/api/interfaces/manager.ts
+++ Server/src/api/interfaces/manager.ts
@@ -5,12 +5,14 @@ import assert from "assert";
 type DBRow = {
     ID:string,
 }
+const xcKeys = ['ID', 'FNODE', 'FPORT', 'TNODE', 'TPORT']
 type XC = DBRow & {
     FNODE: string,
     FPORT: number,
     TNODE: string,
     TPORT: number
 }
+const nodeKeys = ['ID', 'SERIALNUM', 'LOCSTRING', 'PORTCOUNT'];
 type NODE = DBRow & {
     SERIALNUM: string,
     LOCSTRING: string,
@@ -57,6 +59,38 @@ export default class Manager {
         fetcher<T extends DBRow>(dbref:DatabaseReference, tableName:string, column:string):(searchvalue: string) => Promise<(T)[] | null> {
             return async (searchvalue:string) => {
                 return (await dbref.all(`SELECT * FROM ${tableName} WHERE ${column} = "${searchvalue}"`) || null)
+            }
+        },
+        setter<T extends DBRow>(dbref:DatabaseReference):(val:T|T[])=>Promise<void> {
+            return async(val:T|T[]) => {
+                if (
+                    Object.keys(val).every((item) => xcKeys.includes(item)) &&
+                    xcKeys.every((item) => Object.keys(val).includes(item))
+                ) {
+                    //  val is XC
+                    const rowobj:XC = val as unknown as XC;//HACK THIS IS TYPE SAFE
+                    return dbref.exec(/*sql*/`INSERT INTO XCDB (ID, FNODE, FPORT, TNODE, TPORT) VALUES (
+                        "${rowobj.ID}",
+                        "${rowobj.FNODE}",
+                        "${rowobj.FPORT}",
+                        "${rowobj.TNODE}",
+                        "${rowobj.TPORT}"
+                    );`);
+
+                }
+                else if (
+                    Object.keys(val).every((item) => nodeKeys.includes(item)) &&
+                    nodeKeys.every((item) => Object.keys(val).includes(item))
+                ) {
+                    //val is node
+                    const rowobj:NODE = val as unknown as NODE;//HACK THIS IS TYPE SAFE
+                    return dbref.exec(/*sql*/`INSERT INTO NODEDB (ID, SERIALNUM, LOCSTRING, PORTCOUNT) VALUES (
+                        "${rowobj.ID}",
+                        "${rowobj.SERIALNUM}",
+                        "${rowobj.LOCSTRING}",
+                        "${rowobj.PORTCOUNT||'NULL'}"
+                    );`);
+                }
             }
         }
     }
